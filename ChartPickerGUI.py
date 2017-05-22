56a02ac488dffd42523c71456cfda9ede4a08e8f
skywhat <i@skywhat.com>
2017-05-22 19:15:29 +0800

    add more comments, and optimize the code.

--- ChartPickerGUI.py
+++ ChartPickerGUI.py
@@ -18,7 +18,9 @@ import csv
 class GraphFrame(wx.Frame):
     def __init__(self):
         wx.Frame.__init__(self, None, -1, title=u'ChartPicker')
-        #path = "samples.csv"
+        #if you import the csvfile for the first time.
+        #we would check it in function. on_button_import_click_event
+        self.FIRST_TIME=True
         self.create_main_panel()
 
     def create_main_panel(self):
@@ -33,6 +35,8 @@ class GraphFrame(wx.Frame):
 
         self.vbox.Add(self.canvas,1,flag=wx.LEFT | wx.TOP | wx.GROW)
 
+        #All figure windows come with a navigation toolbar, which can be used to navigate through the data set.
+        #https://matplotlib.org/users/navigation_toolbar.html
         self.toolbar = NavigationToolbar(self.canvas)
         self.vbox.Add(self.toolbar,0,wx.EXPAND)
 
@@ -51,6 +55,8 @@ class GraphFrame(wx.Frame):
 
         self.hbox2=wx.BoxSizer(wx.HORIZONTAL)
 
+        #A list control presents lists in a number of formats
+        #https://wxpython.org/Phoenix/docs/html/wx.ListCtrl.html
         self.list_ctrl = wx.ListCtrl(self.panel, size=(-1, 100),
                                      style=wx.LC_REPORT
                                            | wx.BORDER_SUNKEN
@@ -83,6 +89,7 @@ class GraphFrame(wx.Frame):
         self.panel.SetSizer(self.vbox)
         self.vbox.Fit(self)
 
+    #get the datasets from csvfile you have picked
     def get_data(self, path):
         #CSV file data
         self.data_points=data_csv.data(path)
@@ -95,6 +102,7 @@ class GraphFrame(wx.Frame):
         self.datenums=md.date2num(self.dates)
         # CSV file data
 
+    #set how to present x axis
     def set_x_axis(self):
         #Set the format of x axis
         self.xlabels=['Time... %i' % i for i in self.datenums]
@@ -104,42 +112,46 @@ class GraphFrame(wx.Frame):
         self.axes.xaxis.set_major_formatter(self.xfmt)
         # Set the format of x axis
 
+    #how to deal with the point you have clicked
     def on_pick(self,event):
+        #get the index of this point
         self.index = event.ind
-        #self.pick_data.append([np.take(self.data_points.dates, self.index),np.take(self.values, self.index)])
         self.pick_data.append([self.data_points.dates[self.index[0]],self.values[self.index[0]]])
-
 
         #print 'onpick:', self.index, np.take(self.data_points.dates, self.index), np.take(self.values, self.index)
         point_item="Point %s" % self.num
         new_index=self.list_ctrl.InsertItem(self.num, point_item)
-        #self.list_ctrl.SetItem(self.num,0,self.num)
+
         self.list_ctrl.SetItem(new_index, 1,self.pick_data[-1][0])
         self.list_ctrl.SetItem(new_index, 2, self.pick_data[-1][1])
+        #count the point you have clicked
         self.num += 1
 
-
+    #how to deal with the file you have picked
     def on_button_import_click_event(self, event):
+        if self.FIRST_TIME==True:
+            self.FIRST_TIME=False
+        else:
+            # clear the figure
+            self.axes.lines.pop(0)
+            self.axes.clear()
         self.path = self.m_filePicker.GetPath()
         self.get_data(self.path)
         self.set_x_axis()
 
+        # There are a variety of meanings of the picker property
+        # https://matplotlib.org/examples/event_handling/pick_event_demo.html
+        # markers example
+        # https://matplotlib.org/examples/lines_bars_and_markers/marker_reference.html
         self.line=self.axes.plot(self.datenums, self.values, '_', marker=r'8',picker=1.5)
-
+        #https://github.com/joferkington/mpldatacursor
+        #mpldatacursor provides interactive "data cursors" (clickable annotation boxes) for matplotlib.
         datacursor(self.line)
-        self.fig.canvas.mpl_connect('pick_event', self.on_pick)
-                #collect the data you have clicked
+        #collect the data you have clicked
         self.pick_data=[]
-        #self.axes.line.pop(0).remove()
-        self.axes.lines.pop(0)
-        self.axes.clear()
-
         self.get_data(self.path)
         self.set_x_axis()
 
-        self.line=self.axes.plot(self.datenums, self.values, '_', marker=r'8',picker=1.5)
-
-        datacursor(self.line)
         self.fig.canvas.mpl_connect('pick_event', self.on_pick)
         self.canvas.draw()
         self.Update()
